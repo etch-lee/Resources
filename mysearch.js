3cdb633d79e078f8dac5a72ddb8dda503cfb2fe1
chingongouflex <chingongouflex@gmail.com>
2014-09-05 23:38:46 +0100

    Create mysearch.js

---
+++ mysearch.js
@@ -0,0 +1,24 @@
+<script>
+  (function() {
+    var cx = '005091037095330014833:tj-rrxn8hme';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+
+<style>
+.gsc-control-cse {
+font-family: Arial, sans-serif;
+background-color: transparent;
+}
+
+.cse .gsc-control-cse, .gsc-control-cse {
+background-color: #fff;
+}
+</style>
